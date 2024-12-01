a5575fd294f6d4f67136fa8b1d012e1528a19862
TheSaiTeja <iamsaiteja2411@gmail.com>
2024-12-01 20:52:30 +0530

    "Updated init/index.js to use environment variable for MongoDB URL and added dotenv config; updated package-lock.json with new engines node version"

--- init/index.js
+++ init/index.js
@@ -12,8 +12,10 @@
 const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
+require("dotenv").config({ path: "../.env" });
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const dbUrl = process.env.ATLASDB_URL;
 
 main()
   .then(() => {
@@ -24,7 +26,7 @@ main()
   });
 
 async function main() {
-  await mongoose.connect(MONGO_URL);
+  await mongoose.connect(dbUrl);
 }
 
 const initDB = async () => {
